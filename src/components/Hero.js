2a5b3c7a295158edf7b7bd4052ae2e5482a6413a
Ztaclie <melmeh42@gmail.com>
2024-12-09 22:38:24 +0300

    added tailwindcss

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,43 +2,10 @@ import "./Hero.css";
 import kebab1 from "../img/kebab1.jpg";
 
 function Hero() {
-  const heroStyle = {
-    backgroundImage: `URL(${kebab1})`,
-
-    /* Set a specific height */
-    minHeight: "100vh",
-
-    /* Create the parallax scrolling effect */
-    backgroundAttachment: "fixed",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    position: "relative",
-  };
-
   return (
     <div className="Hero">
-      <div style={heroStyle}>
-        <div className="position-absolute" style={{ top: "20%", left: "3%" }}>
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <span
-                  className="text-primary fs-1"
-                  style={{ borderColor: "#ffa500" }}
-                >
-                  miniPiknik Restaurant
-                </span>
-              </div>
-            </div>
-            <div className="row mt-5">
-              <div className="col-12">
-                <span className="fs-1 btn-outline-primary">Bizi Arayın</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <h1>Delicious Kebabs</h1>
+      <img src={kebab1} alt="A delicious kebab" />
     </div>
   );
 }
