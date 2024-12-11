5c941159ff80580632c9a8d7e23e141b5442f1a3
Ztaclie <melmeh42@gmail.com>
2024-12-11 21:44:43 +0300

    fixed hero width and height

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,8 @@ import kebab1 from "../img/kebab1.jpg";
 
 function Hero() {
   return (
-    <div className="Hero relative w-96">
-      <div className="absolute top-0 left-0 z-10 text-white">
-        <h1>Delicious Kebabs</h1>
-      </div>
-      <img
-        className="w-max absolute top-0 left-0 z-0"
-        src={kebab1}
-        alt="A delicious kebab"
-      />
+    <div className="Hero w-full">
+      <img className="w-auto h-auto" src={kebab1} alt="A delicious kebab" />
     </div>
   );
 }
