79e0fab2a70c07ebec8261ef93947d50a5302079
Ztaclie <melmeh42@gmail.com>
2024-12-12 20:03:42 +0300

    fixed image position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,19 @@
 import "./Hero.css";
 import kebab1 from "../img/kebab1.jpg";
 
+const heroStyle = {
+  backgroundImage: `URL(${kebab1})`,
+
+  /* Create the parallax scrolling effect */
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+};
+
 function Hero() {
   return (
-    <div className="Hero w-full">
-      <img className="w-auto h-auto" src={kebab1} alt="A delicious kebab" />
+    <div className="Hero w-full min-h-screen" style={heroStyle}>
+      <h1 className="text-white">Mini Piknik</h1>
     </div>
   );
 }
